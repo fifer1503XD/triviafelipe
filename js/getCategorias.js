b390f6ab7867d2e07afed3e5729af089e9431aa8
fiferortiz <pedrofelipeortiz@gmail.com>
2020-10-14 15:09:50 -0500

    subi archivos

---
+++ js/getCategorias.js
@@ -0,0 +1,17 @@
+
+export function getCategories() {
+    const questionsQuantity = document.getElementById('questions-number').value
+    fetch(`https://opentdb.com/api_category.php`)
+        .then(response => response.json())
+        .then(data => Categories(data.trivia_categories))  
+}
+
+function Categories(data){
+    const categories = document.getElementById('questions-category')
+    data.forEach(data=>{
+        const Category = `<option id"${data.id}" value="${data.id}">${data.name}</option>`
+        categories.innerHTML += Category;
+    })
+    console.log(data)
+}
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-function getValuesQuestion(){
+
+    function getValuesQuestion(){
     const questionsQuantity = document.getElementById('questions-number').value
     const questionsCategory= document.getElementById('questions-category').value
     const questionsDifficulty= document.getElementById('difficulty').value
@@ -7,74 +8,58 @@ function getValuesQuestion(){
     getQuestions(questionsQuantity,questionsCategory,questionsDifficulty,questionType);
     return questionsQuantity,questionsCategory,questionsDifficulty,questionType
 }
-function getQuestions(questionsQuantity, questionsCategory,questionsDifficulty,questionType) {
-    alert('ingresa a pintar preguntas')
+   function getQuestions(questionsQuantity, questionsCategory,questionsDifficulty,questionType) {
     fetch(`https://opentdb.com/api.php?amount=${questionsQuantity}&category=${questionsCategory}&difficulty=${questionsDifficulty}&type=${questionType}`)    
     .then(response => response.json())
         .then(data => printCards(data.results))
-}
-        // .then(data => printCards(data.results))
-
-function getCategories() {
-    const questionsQuantity = document.getElementById('questions-number').value
-    fetch(`https://opentdb.com/api_category.php`)
-        .then(response => response.json())
-        .then(data => Categories(data.trivia_categories))  
-}
-
-function Categories(data){
-    const categories = document.getElementById('questions-category')
-    data.forEach(data=>{
-        const Category = `<option id"${data.id}" value="${data.id}">${data.name}</option>`
-        categories.innerHTML += Category;
-    })
-    console.log(data)
-}
-
-// console.log(datos);
-
-function printCards(questions) {
+     }
+   
+    let identificador=0;
+   function printCards(questions) {
+     
     const container = document.getElementById('container-cards');
     container.innerHTML = '';
-    questions.forEach(question => {
-        const card = returnCardHTML(question);
+    questions.forEach((question,index) => {
+        const card = returnCardHTML(question,index);
         container.innerHTML += card;
     });
-    // poner las preguntas en mi página web
+    // poner las preguntas en mi pÃ¡gina web
 }
 
-function returnCardHTML(q) {
+function returnCardHTML(q,index) {
     const card = `<div class="card">
                     <div class="card-body">
                     <h5 class="card-title">${q.category}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
-                        ${returnAnswersHTML(q.correct_answer, q.incorrect_answers)}           
+                        ${returnAnswersHTML(q.correct_answer, q.incorrect_answers,index)}           
                     </div>
                 </div>`
-    return card;
+    return card
 }
 
-
-function returnAnswersHTML(correct, incorrects) {
-    const correctHTML = `<div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            ${correct}
-                            </label>
-                        </div>`;
-
-
+function returnAnswersHTML(correct, incorrects,ind) {
+    incorrects.push(correct)
     let incorrectHTML = '';
-    incorrects.forEach((incorrect) => {
-        incorrectHTML += `<div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
+    incorrects.forEach((incorrect,index) => {
+        incorrectHTML += `  <div>
+                            <div class="form-check">
+                            <input class="form-check-input" type="radio" name="answer-${ind}" id="answer-${index}${ind}" value="${index}${ind}">
+                            <label class="form-check-label" for="answer-${index}${ind}">
                             ${incorrect}
                             </label>
+                        </div>
                         </div>`;
+                       
+                      
     })
+    
 
 
-    return correctHTML + incorrectHTML;
+    return incorrectHTML;
 }
+getValuesQuestion()
+import { getCategories} from './getCategorias.js'
+getValuesQuestion()    
 getCategories()
+
+window.getValuesQuestion = getValuesQuestion
